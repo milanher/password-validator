44e6d484db44640b109a67a5af1f1d96d9f8a48d
milanher <milan.hermange@bettyblocks.com>
2024-11-12 09:39:20 +0100

    added the code

---
+++ password/1.0/index.js
@@ -0,0 +1,46 @@
+const password = async ({ Password, minLength, maxLength, specialCharacter, upperCase, lowerCase}, customRegex) => {
+    const checkPassword = (password) => {
+        isValid = true
+        strength
+        if (minLength || maxLength) {
+            const maxlengthvalid = password.length <= maxLength ? true : false
+            const minlengthvalid = password.length >= minLength ? true : false
+            if (!(minlengthvalid && maxlengthvalid)) {
+                isValid = false
+            }
+        }
+        if (specialCharacter) {
+            const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+            if (!specialCharRegex.test(password)) {
+                isValid = false
+            };
+        }
+        if (lowerCase) {
+            const hasLower = [...password].some(char => char === char.toLowerCase() && char !== char.toUpperCase());
+            if (!hasLower) {
+                isValid = false
+            }
+        }
+        if (upperCase) {
+            const hasUpper = [...password].some(char => char === char.toUpperCase() && char !== char.toLowerCase());
+            if (!hasUpper) {
+                isValid = false
+            }
+        }
+        if (customRegex) {
+            const regex = customRegex
+            if (!regex.test(password)) {
+                isValid = false
+            }
+        }
+
+        return {
+            isValid
+        }
+    }
+    return {
+        result: checkPassword(Password)
+    }
+}
+
+export default password;
