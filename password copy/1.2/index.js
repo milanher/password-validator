668c694efc97b339a34b10963fd15157b7bf7751
milanher <milan.hermange@bettyblocks.com>
2024-11-15 11:31:44 +0100

    com

---
+++ password copy/1.2/index.js
@@ -0,0 +1,62 @@
+const password = async ({ Password, digits, minLength, maxLength, specialCharacter, upperCase, lowerCase, SpecifiedChar, customRegex }) => {
+    const checkPassword = (password) => {
+        isValid = true
+        if (minLength || maxLength) {
+            const maxlengthvalid = password.length <= maxLength ? true : false
+            const minlengthvalid = password.length >= minLength ? true : false
+            if (!(minlengthvalid && maxlengthvalid)) {
+                isValid = false
+            }
+        }
+        if (!specialCharacter && SpecifiedChar) {
+            console.warn("You cant check for special digits without checking the special characters checkmark");
+        }
+        if (specialCharacter && SpecifiedChar) {
+            const specialCharRegex = new RegExp(`[${SpecifiedChar}]`);
+            if (!specialCharRegex.test(password)) {
+                isValid = false
+            };
+        }
+        if (specialCharacter && !SpecifiedChar) {
+            const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+            if (!specialCharRegex.test(password)) {
+                isValid = false
+            };
+        }
+        if (lowerCase) {
+            const hasLower = [...password].some(char => char === char.toLowerCase() && char !== char.toUpperCase());
+            if (!hasLower) {
+                isValid = false
+            }
+        }
+        if (upperCase) {
+            const hasUpper = [...password].some(char => char === char.toUpperCase() && char !== char.toLowerCase());
+            if (!hasUpper) {
+                isValid = false
+            }
+        }
+        if (digits) {
+            const hasDigits = (str) => /\d/.test(str);
+            const check = hasDigits(password)
+            if (!check) {
+                isValid = false
+            }
+        }
+
+        if (customRegex) {
+            const regex = new RegExp(customRegex)
+            if (!regex.test(password)) {
+                isValid = false
+            }
+        }
+
+        return {
+            isValid
+        }
+    }
+    return {
+        result: checkPassword(Password)
+    }
+}
+
+export default password;
